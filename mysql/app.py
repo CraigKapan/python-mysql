b52d8eabdcf91baa2205bba99b9880944e7e121c
Craig <CraigKapan@gmail.com>
2021-03-11 12:52:36 +0200

    learned about drop table

---
+++ mysql/app.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE IF EXISTS customers"
+
+mycursor.execute(sql)
